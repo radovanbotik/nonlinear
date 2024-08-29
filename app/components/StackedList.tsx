da84a7fd9dec304bcf4ca8afa974dcca19c89362
radovanbotik <radovanbotik@gmail.com>
2024-08-29 10:24:32 +0200

    change grid layout

--- app/components/StackedList.tsx
+++ app/components/StackedList.tsx
@@ -1,5 +1,6 @@
 import { RiPlayFill } from "react-icons/ri";
 import { RiPlayListAddFill } from "react-icons/ri";
+import { RiMoreFill } from "react-icons/ri";
 
 import Image from "next/image";
 
@@ -15,7 +16,7 @@ export default function StackedList({ data }: { data: ReleaseData[] }) {
       {data.map(release => (
         <li
           key={release._id}
-          className="relative group grid grid-cols-[max-content_1fr_max-content] lg:grid-cols-[max-content_max-content_max-content_1fr_1fr_1fr_max-content] gap-3  bg-gray-700/60 hover:bg-gray-700/70 bg-clip-padding backdrop-filter backdrop-blur-sm "
+          className="relative group grid grid-cols-[max-content_max-content_1fr_max-content] lg:grid-cols-[max-content_max-content_max-content_1fr_1fr_1fr_max-content] gap-3  bg-gray-700/60 hover:bg-gray-700/70 bg-clip-padding backdrop-filter backdrop-blur-sm "
         >
           <div className="relative">
             <Link href={`/releases/${release.slug}`}>
@@ -31,7 +32,7 @@ export default function StackedList({ data }: { data: ReleaseData[] }) {
             </Link>
           </div>
 
-          <div className="my-auto leading-[0] hidden lg:block">
+          <div className="my-auto leading-[0] ">
             <button className="relative leading-0 group/button ">
               <RiPlayFill className="text-gray-300 w-4 h-4" />
               <div className="group-hover/button:opacity-100 absolute opacity-0 transition ease-in-out top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-[0px_0px_16px_3px_#f7fafc]"></div>
@@ -81,38 +82,14 @@ export default function StackedList({ data }: { data: ReleaseData[] }) {
                 download
               </button>
             </div>
-            {/* <div className=" flex text-xs leading-none text-gray-400 mt-1 ">
-              <div className="truncate tracking-tight">{release.catalog_number}</div>
-            </div> */}
           </div>
 
-          <div className="my-auto min-w-4 leading-[0] flex flex-col lg:hidden">
-            <div className="text-xs font-medium  text-gray-300 pr-3">
-              <button className="relative leading-0 group/button ">
-                <RiPlayFill className="text-gray-300 w-4 h-4" />
-                <div className="group-hover/button:opacity-100 absolute opacity-0 transition ease-in-out top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-[0px_0px_16px_3px_#f7fafc]"></div>
-              </button>
-            </div>
+          <div className="my-auto min-w-4 pr-3 leading-[0] flex  gap-1 lg:hidden">
+            <button className="relative leading-0 group/button ">
+              <RiMoreFill className="text-gray-300 w-4 h-4" />
+              <div className="group-hover/button:opacity-100 absolute opacity-0 transition ease-in-out top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-[0px_0px_16px_3px_#f7fafc]"></div>
+            </button>
           </div>
-
-          {/* <div className="flex items-center justify-between gap-x-4  sm:flex-none">
-            <div className="hidden sm:block">
-              <p className="text-sm leading-6 text-gray-900">{person.role}</p>
-              {person.lastSeen ? (
-                <p className="mt-1 text-xs leading-5 text-gray-500">
-                  Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
-                </p>
-              ) : (
-                <div className="mt-1 flex items-center gap-x-1.5">
-                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-                  </div>
-                  <p className="text-xs leading-5 text-gray-500">Online</p>
-                </div>
-              )}
-            </div>
-            <ChevronRightIcon aria-hidden="true" className="h-5 w-5 flex-none text-gray-400" />
-          </div> */}
         </li>
       ))}
     </ul>
